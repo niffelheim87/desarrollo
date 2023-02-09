0151c4def13826de0aa73d20166d6b7e8b47372b
niffelheim87 <omarkali@gmail.com>
2023-02-09 20:17:44 +0100

    Añadida relacion many2many, tabla intermedia, gestion de prestamos y constraints de fechas

---
+++ volumesOdoo/addons/biblioteca_comics/models/biblioteca_prestamos.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class BibliotecaPrestamos(models.Model):
+    _name = 'biblioteca.prestamos'
+    _description = 'Prestamos'
+    socio_id = fields.Many2one('biblioteca.socios', string='Socio', required=True)
+    comic_id = fields.Many2one('biblioteca.comics', string='Cómic', required=True)
+    fecha_prestamo = fields.Date(string='Fecha de préstamo', required=True)
+    fecha_devolucion = fields.Date(string='Fecha de devolución', required=True)
+    
+    @api.constrains('fecha_prestamo')
+    def _check_fecha_prestamo(self):
+        for prestamo in self:
+            if prestamo.fecha_prestamo > fields.Date.today():
+                raise models.ValidationError('La fecha de prestamo no puede ser posterior a la fecha actual.')
+            
+    @api.constrains('fecha_devolucion')
+    def _check_fecha_devolucion(self):
+        for prestamo in self:
+            if prestamo.fecha_devolucion < fields.Date.today():
+                raise models.ValidationError('La fecha de prestamo no puede ser anterior a la fecha actual.')
--- volumesOdoo/addons/biblioteca_comics/models/biblioteca_socios.py
+++ volumesOdoo/addons/biblioteca_comics/models/biblioteca_socios.py
@@ -1,11 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class BibliotecaSocio(models.Model):
     _name = 'biblioteca.socios'
     _description = 'Socios'
+    _rec_name = 'full_name'
 
     id = fields.Integer(string='ID', required=True)
     nombre = fields.Char(string='Nombre')
-    apellidos = fields.Char(string='Apellidos')
-    comic_id = fields.One2many('biblioteca.comics', 'socio_id', string='Comics')
+    apellido = fields.Char(string='Apellido')
+    full_name = fields.Char(string='Nombre completo', compute='_compute_full_name')
+    comic_id = fields.Many2many('biblioteca.comics', string='Comics', through='biblioteca.prestamos')
+
+    @api.depends('nombre', 'apellido')
+    def _compute_full_name(self):
+        for record in self:
+            record.full_name = record.nombre + ' ' + record.apellido
